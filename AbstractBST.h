0417a60e709cf94096149a8c22ba30e905ba118e
Aakriti <banjarabinda@gmail.com>
2022-11-20 22:00:47 +0545

    lab4

--- AbstractBST.h
+++ AbstractBST.h
@@ -1,14 +1,12 @@
-#ifndef AbstractBST_h
-#define AbstractBST_h
+#ifndef ABSTRACTBST_h
+#define ABSTRACTBST_h
 
 class AbstractBST
 {
 public:
-    virtual bool isEmpty{} = 0;
-    virtual void add{int key, int value} = 0;
-    virtual void max{int &output} = 0;
-    virtual void min{int &output} = 0;
-    virtual void exists(int targetkey) = 0;
-    virtual void inorder{} = 0;
+    virtual bool isEmpty() = 0;
+    virtual void add(int key, int value) = 0;
+    virtual void remove(int key) = 0;
+    virtual bool search(int key) = 0;
 };
 #endif
